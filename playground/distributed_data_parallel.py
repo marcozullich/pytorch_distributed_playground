e412a8d317db227427fe9febb51dabe1e40c4a5a
marcozullich <zuzzu90@gmail.com>
2021-06-17 17:03:12 +0200

    correction on

--- playground/distributed_data_parallel.py
+++ playground/distributed_data_parallel.py
@@ -6,22 +6,6 @@ from helper_scripts.datasets import MNIST
 from helper_scripts.models import MLP
 from helper_scripts.train import train
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-n', '--nodes', default=1, type=int, metavar='N')
-    parser.add_argument('-g', '--gpus', default=1, type=int, help='number of gpus per node')
-    parser.add_argument('-nr', '--nr', default=0, type=int, help='ranking within the nodes')
-    parser.add_argument('--epochs', default=2, type=int, help='number of total epochs to run')
-    parser.add_argument("--batch_size", default=128, type=int, help="batch size for trainset")
-    args = parser.parse_args()
-
-    args.world_size = args.gpus * args.nodes
-
-    os.environ["MASTER_ADDR"] = "10.57.23.164"
-    os.environ["MASTER_PORT"] = "8888"
-
-    torch.multiprocessing.spawn(prepare_train, nprocs=args.gpus, args = (args,))
-
 def prepare_train(gpu, args):
     rank = args.gpus * args.nr + gpu
     torch.distirbuted.init_process_group(
@@ -46,5 +30,19 @@ def prepare_train(gpu, args):
     train(net, optimizer, loss_fn, args.num_epochs, trainloader, device, cuda_non_blocking=True)
 
 
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-n', '--nodes', default=1, type=int, metavar='N')
+    parser.add_argument('-g', '--gpus', default=1, type=int, help='number of gpus per node')
+    parser.add_argument('-nr', '--nr', default=0, type=int, help='ranking within the nodes')
+    parser.add_argument('--epochs', default=3, type=int, help='number of total epochs to run')
+    parser.add_argument("--batch_size", default=128, type=int, help="batch size for trainset")
+    args = parser.parse_args()
 
+    args.world_size = args.gpus * args.nodes
+
+    os.environ["MASTER_ADDR"] = "10.57.23.164"
+    os.environ["MASTER_PORT"] = "8888"
+
+    torch.multiprocessing.spawn(prepare_train, nprocs=args.gpus, args = (args,))
 
