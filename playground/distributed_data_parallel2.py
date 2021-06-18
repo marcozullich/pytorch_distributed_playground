c4b927d3a78481c7ffbfcab351ae4d29b86903a3
marcozullich <zuzzu90@gmail.com>
2021-06-18 13:20:48 +0200

    addition on DDP2

--- playground/distributed_data_parallel2.py
+++ playground/distributed_data_parallel2.py
@@ -99,7 +99,7 @@ def _distributed_worker(
 
 
 def prepare_train(args):
-    #print(f"Namespace {args}")
+    print(f"Node {comm.get_rank()}")
     device = f"cuda:{comm.get_local_rank()}"
     net = MLP().to(device)
     net = torch.nn.parallel.DistributedDataParallel(net, device_ids=[device], broadcast_buffers=False)
